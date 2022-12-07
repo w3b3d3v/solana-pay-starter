4be53e0d6e0134667557c4ebd45fd8b4a5940892
vitordev <vitor.mancio@gmail.com>
2022-12-07 07:27:10 -0300

    transalate and adapt meta tags to web3dev version

--- components/Head.js
+++ components/Head.js
@@ -9,21 +9,20 @@ export default function HeadComponent() {
 
       <title>Solana Pay Store</title>
       <meta name="title" content="Solana Pay Store" />
-      <meta name="description" content="Buy items on my store using Solana Pay!" />
+      <meta name="description" content="Compre itens na minha loja usando Solana Pay!" />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://buildspace.so/" />
+      <meta property="og:url" content="https://bootcamp.web3dev.com.br/" />
       <meta property="og:title" content="Solana Pay Store" />
-      <meta property="og:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="og:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="og:description" content="Compre itens na minha loja usando Solana Pay!" />
+      <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_Pay_Store_256.jpeg?alt=media&token=c1e28101-d7e2-4512-9f5b-b199e6df10e4" />
 
       {/* Twitter */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://buildspace.so/" />
+      <meta property="twitter:url" content="https://bootcamp.web3dev.com.br/" />
       <meta property="twitter:title" content="Solana Pay Store" />
-      <meta property="twitter:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="twitter:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="twitter:description" content="Compre itens na minha loja usando Solana Pay!" />
+      <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_Pay_Store_256.jpeg?alt=media&token=c1e28101-d7e2-4512-9f5b-b199e6df10e4" />
     </Head>
   );
 }
